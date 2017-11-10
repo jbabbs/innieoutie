c637e24c76793d68b56240861c2abc8b3e8e1363
Justin Babbs <justin.babbs@gmail.com>
2017-11-10 17:21:48 -0500

    added some mockups, removed the router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,6 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-
-import { AppRoutingModule } from './app-routing.module';
 
 import { ElectronService } from './services/electron.service';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -18,19 +15,19 @@ import { DatabaseService } from './services/database.service';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule,
     NgbModule.forRoot(),
   ],
   providers: [
     ElectronService,
     DatabaseService,
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
